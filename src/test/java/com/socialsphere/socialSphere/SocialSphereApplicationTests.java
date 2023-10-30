74323f9fc2b7f7724a4cc2adead70df4628a2aaa
Bruno <moreirasbr@gmail.com>
2023-10-30 18:47:19 -0300

    initial commit

---
+++ src/test/java/com/socialsphere/socialSphere/SocialSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialsphere.socialSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
